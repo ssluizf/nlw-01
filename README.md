185a1bcd991ef7300dbc84b5e61f27709f323242
LuizFernando55 <45644952+LuizFernando55@users.noreply.github.com>
2020-06-29 01:34:43 -0300

    README update

---
+++ README.md
@@ -0,0 +1,12 @@
+# Next Level Week
+Made project from Next Level Week #01 of :rocket: [Rocketseat](https://rocketseat.com.br/).
+
+# App
+### Main page
+<img src=".github/web_index" alt="Main page" />
+
+### Cadastro
+<img src=".github/web_cadastro" alt="Register page" />
+
+### Mobile
+<img src=".github/mobile" alt="Mobile" />
--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -129,19 +129,18 @@ const CreatePoint = () => {
 
         const data = new FormData();
 
+        data.append('name', name);
+        data.append('email', email);
+        data.append('whatsapp', whatsapp);
+        data.append('uf', uf);
+        data.append('city', city);
+        data.append('latitude', String(latitude));
+        data.append('longitude', String(longitude));
+        data.append('items', items.join(','));
 
-            data.append('name', name);
-            data.append('email', email);
-            data.append('whatsapp', whatsapp);
-            data.append('uf', uf);
-            data.append('city', city);
-            data.append('latitude', String(latitude));
-            data.append('longitude', String(longitude));
-            data.append('items', items.join(','));
-
-            if (selectedFile) {
-                data.append('image', selectedFile)
-            }
+        if (selectedFile) {
+            data.append('image', selectedFile)
+        }
 
         await api.post('points', data);
 
@@ -174,10 +173,10 @@ const CreatePoint = () => {
                     <div className="field">
                         <label htmlFor="name">Nome da entidade</label>
                         <input
-                        type="text"
-                        name="name"
-                        id="name"
-                        onChange={handleInputChange}
+                            type="text"
+                            name="name"
+                            id="name"
+                            onChange={handleInputChange}
                         />
                     </div>
 
@@ -185,19 +184,19 @@ const CreatePoint = () => {
                         <div className="field">
                             <label htmlFor="email">E-mail</label>
                             <input
-                            type="email"
-                            name="email"
-                            id="email"
-                            onChange={handleInputChange}
+                                type="email"
+                                name="email"
+                                id="email"
+                                onChange={handleInputChange}
                             />
                         </div>
                         <div className="field">
                             <label htmlFor="whatsapp">Whatsapp</label>
                             <input
-                            type="text"
-                            name="whatsapp"
-                            id="whatsapp"
-                            onChange={handleInputChange}
+                                type="text"
+                                name="whatsapp"
+                                id="whatsapp"
+                                onChange={handleInputChange}
                             />
                         </div>
                     </div>
